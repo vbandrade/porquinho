5dc0cc428f6c23e5c8266bab2b36565ceb616d5c
@vbandrade <vbandrade@gmail.com>
2018-10-18 23:46:49 -0300

    Revert "utilizando built_value no enum EntryType"
    This reverts commit b00d0e56e612989b56a7c3bab63fa4753f1716c2.

--- app/lib/src/models/entry.dart
+++ app/lib/src/models/entry.dart
@@ -1,25 +1,13 @@
 import "package:intl/intl.dart";
 import 'package:money/money.dart';
-import 'package:built_collection/built_collection.dart';
-import 'package:built_value/built_value.dart';
-import 'package:built_value/serializer.dart';
 import 'package:app/src/models/account.dart';
 import 'package:app/src/models/category.dart';
 
-part "entry.g.dart";
-
-class EntryType extends EnumClass {
-  static Serializer<EntryType> get serializer => _$entryTypeSerializer;
-  static const EntryType credit = _$credit;
-  static const EntryType debit = _$debit;
-  static const EntryType transfer_in = _$transfer_in;
-
-  static const EntryType transfer_out = _$transfer_out;
-
-  const EntryType._(String name) : super(name);
-
-  static BuiltSet<EntryType> get values => _$stValues;
-  static EntryType valueOf(String name) => _$stValueOf(name);
+enum EntryType {
+  credit,
+  debit,
+  transfer_in,
+  transfer_out,
 }
 
 class Entry {
