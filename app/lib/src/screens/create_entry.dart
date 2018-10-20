871475b1482709e2c013f052d0bdc94d37c260b2
@vbandrade <vbandrade@gmail.com>
2018-10-20 13:04:41 -0300

    estrutura basica da tela

--- app/lib/src/screens/create_entry.dart
+++ app/lib/src/screens/create_entry.dart
@@ -4,10 +4,30 @@ class CreateEntryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Criar Transação")),
-      body: Container(
-        color: Colors.orange,
+      appBar: AppBar(
+        title: const Text('Criar transação'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
       ),
+      body: ListView(children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          child: RaisedButton(
+            child: Text(
+              'Salvar',
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () {},
+            color: Colors.blue,
+          ),
+        )
+      ]),
     );
   }
 }
