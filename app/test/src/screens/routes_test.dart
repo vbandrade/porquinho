d13f9b5f7c81393edad0b91e52187435b0e1d028
@vbandrade <vbandrade@gmail.com>
2018-10-20 23:42:55 -0300

    refatoracao de teste

--- app/test/src/screens/routes_test.dart
+++ app/test/src/screens/routes_test.dart
@@ -1,16 +1,20 @@
+import 'package:app/src/blocs/monthly_expenses_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:app/src/blocs/provider.dart';
 import 'package:app/src/screens/routes.dart' as routes;
 import 'package:app/src/screens/monthly_expenses.dart';
 import 'package:app/src/screens/create_entry.dart';
 
+Type _typeOf<T>() => T;
+
 void main() {
   group("Test routes", () {
     ///TODO: Use WidgetTester
     test("homeHandler returns MonthlyExpensesScreen", () {
       Provider result = routes.homeHandler.handlerFunc(null, null);
+      final type = _typeOf<Provider<MonthlyExpensesBloc>>();
 
-      expect(result.runtimeType.toString(), "Provider<MonthlyExpensesBloc>");
+      expect(result.runtimeType, type);
       expect(result.child.runtimeType, MonthlyExpensesScreen);
     });
 
