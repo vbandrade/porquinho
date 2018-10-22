08e37be34207ab4ece04b7654d2574643fce3ad5
@vbandrade <vbandrade@gmail.com>
2018-10-22 00:00:01 -0300

    criacao do helper de enteies

--- app/test/src/helpers.dart
+++ app/test/src/helpers.dart
@@ -1,3 +1,4 @@
+import 'package:app/src/models/entry.dart';
 import 'package:flutter/material.dart';
 
 class Wrapper extends StatelessWidget {
@@ -12,5 +13,47 @@ class Wrapper extends StatelessWidget {
         body: child,
       ),
     );
+  }
+}
+
+class EntriesHelper {
+  static Map<String, dynamic> getEntryMap({
+    double amount = 3.14,
+    EntryType type = EntryType.debit,
+  }) {
+    Map<String, dynamic> categoryMap = Map<String, dynamic>();
+    categoryMap["name"] = "Sports";
+
+    Map<String, dynamic> account = Map<String, dynamic>();
+    account["name"] = "cashhh";
+    account["type"] = "EntryAccountType.cash";
+
+    return {
+      "type": type.toString(),
+      "amount": amount,
+      "date": DateTime.now(),
+      "category": categoryMap,
+      "description": "lallalalalla",
+      "account": account
+    };
+  }
+
+  static Entry getDebit(double value) {
+    return Entry.fromMap("id1234", getEntryMap(amount: value));
+  }
+
+  static Entry getCredit(double value) {
+    return Entry.fromMap(
+        "id1234", getEntryMap(amount: value, type: EntryType.credit));
+  }
+
+  static Entry getTransferIn(double value) {
+    return Entry.fromMap(
+        "id1234", getEntryMap(amount: value, type: EntryType.transfer_in));
+  }
+
+  static Entry getTransferOut(double value) {
+    return Entry.fromMap(
+        "id1234", getEntryMap(amount: value, type: EntryType.transfer_out));
   }
 }
--- app/test/src/models/entry_test.dart
+++ app/test/src/models/entry_test.dart
@@ -1,40 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:app/src/models/entry.dart';
 import 'package:money/money.dart';
+import 'package:app/src/models/entry.dart';
+import "../helpers.dart";
 
 void main() {
   group("Test Entry ", () {
-    Map<String, dynamic> _getEntryMap({
-      double amount = 3.14,
-      EntryType type = EntryType.debit,
-    }) {
-      Map<String, dynamic> categoryMap = Map<String, dynamic>();
-      categoryMap["name"] = "Sports";
-
-      Map<String, dynamic> account = Map<String, dynamic>();
-      account["name"] = "cashhh";
-      account["type"] = "EntryAccountType.cash";
-
-      return {
-        "type": type.toString(),
-        "amount": amount,
-        "date": DateTime.now(),
-        "category": categoryMap,
-        "description": "lallalalalla",
-        "account": account
-      };
-    }
-
     test("Amount string should ", () {
-      final entryMap = _getEntryMap();
+      final entryMap = EntriesHelper.getEntryMap();
       final entry = Entry.fromMap("id1234", entryMap);
 
       expect(entry.amountString, "-R\$ 3,14");
     });
 
     test('sum of debit entries should be negative', () {
-      final entryMap = _getEntryMap();
+      final entryMap = EntriesHelper.getEntryMap();
       List<Entry> entries = [
         Entry.fromMap("id1234", entryMap),
         Entry.fromMap("id1234", entryMap),
@@ -51,29 +30,10 @@ void main() {
       expect(result, Money.fromDouble(-12.56, Currency("BRL")));
     });
 
-    Entry _getDebit(double value) {
-      return Entry.fromMap("id1234", _getEntryMap(amount: value));
-    }
-
-    Entry _getCredit(double value) {
-      return Entry.fromMap(
-          "id1234", _getEntryMap(amount: value, type: EntryType.credit));
-    }
-
-    Entry _getTransferIn(double value) {
-      return Entry.fromMap(
-          "id1234", _getEntryMap(amount: value, type: EntryType.transfer_in));
-    }
-
-    Entry _getTransferOut(double value) {
-      return Entry.fromMap(
-          "id1234", _getEntryMap(amount: value, type: EntryType.transfer_out));
-    }
-
     test('sum of  entries according to type', () {
       List<Entry> entries = [
-        _getDebit(3.14),
-        _getCredit(4.19),
+        EntriesHelper.getDebit(3.14),
+        EntriesHelper.getCredit(4.19),
       ];
 
       final result = entries.fold<Money>(Money.fromDouble(0.0, Currency("BRL")),
@@ -85,10 +45,10 @@ void main() {
     });
     test('sum of entries of every to type', () {
       List<Entry> entries = [
-        _getDebit(3.14),
-        _getCredit(4.19),
-        _getTransferIn(2.22),
-        _getTransferOut(3.33),
+        EntriesHelper.getDebit(3.14),
+        EntriesHelper.getCredit(4.19),
+        EntriesHelper.getTransferIn(2.22),
+        EntriesHelper.getTransferOut(3.33),
       ];
 
       final result = entries.fold<Money>(Money.fromDouble(0.0, Currency("BRL")),
