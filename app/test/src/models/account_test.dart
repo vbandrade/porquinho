7fe89d56c02ad04d4802bdf0c381097e710a220a
@vbandrade <vbandrade@gmail.com>
2018-10-22 23:49:18 -0300

    remocao de teste

--- app/test/src/models/account_test.dart
+++ app/test/src/models/account_test.dart
@@ -1,18 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:app/src/models/account.dart';
-
 void main() {
-  group("Test Account model", () {
-    test("account created from map", () {
-      final map = {
-        "id": "123",
-        "name": "cash",
-        "type": "cash",
-      };
-      final result = Account.fromMap(map);
-
-      expect(result.type, AccountType.cash);
-    });
-  });
+  group("Test Account model", () {});
 }
