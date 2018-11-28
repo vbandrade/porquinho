aaf73cd2972048b99e46d79729156eb02eed51fc
@vbandrade <vbandrade@gmail.com>
2018-11-27 23:47:17 -0200

    criado teste para serializar/deserializar account

--- app/test/src/models/account_test.dart
+++ app/test/src/models/account_test.dart
@@ -1,5 +1,18 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:app/src/models/account.dart';
+import 'package:app/src/models/serializers.dart';
 
 void main() {
-  group("Test Account model", () {});
+  group("Test Account model", () {
+    test('serializer shouldnt throw exception', () {
+      final builder = AccountBuilder()
+        ..name = "category"
+        ..type = AccountType.cash;
+      final account = builder.build();
+      final serialized = serializers.serializeWith(Account.serializer, account);
+      final accountDeserialized =
+          serializers.deserializeWith(Account.serializer, serialized);
+      expect(account, accountDeserialized);
+    });
+  });
 }
