80473beca60146c0ea0da6762b633406989b94f8
@vbandrade <vbandrade@gmail.com>
2018-12-06 09:41:57 -0200

    refatoracao do input

--- app/lib/src/widgets/entry_form/src/description_input.dart
+++ app/lib/src/widgets/entry_form/src/description_input.dart
@@ -1,20 +1,34 @@
 import 'package:app/src/widgets/entry_form/src/styles.dart';
 import 'package:flutter/material.dart';
 
-class DescriptionInput extends StatelessWidget {
+class DescriptionInput extends StatefulWidget {
+  @override
+  DescriptionInputState createState() {
+    return DescriptionInputState();
+  }
+}
+
+class DescriptionInputState extends State<DescriptionInput> {
+  final TextEditingController _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-        style: Styles.userInputStyle,
-        keyboardType: TextInputType.text,
-        decoration: InputDecoration(hintText: "descrição"),
-        autocorrect: true,
-        validator: (value) {
-          if (value.isEmpty) {
-            return 'por favor, insira uma transação';
-          }
-        },
-        onFieldSubmitted: (String value) {},
-        onSaved: (String value) {});
+    return Container(
+        decoration: BoxDecoration(
+            border: Border(bottom: BorderSide(color: Colors.grey[500]))),
+        child: Padding(
+          padding: EdgeInsets.only(bottom: 5.0),
+          child: TextField(
+            style: Styles.userInputStyle,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+            ),
+            controller: _controller,
+            onChanged: (val) {
+              debugPrint(val);
+            },
+            keyboardType: TextInputType.text,
+          ),
+        ));
   }
 }
