7e7fd22ec94b941fd540db3484f1e2b488e011ed
@vbandrade <vbandrade@gmail.com>
2018-12-17 07:50:26 -0200

    criado serializador de Timestamp

--- app/lib/src/models/serializers.dart
+++ app/lib/src/models/serializers.dart
@@ -4,11 +4,42 @@ import 'package:app/src/models/entry.dart';
 import 'package:app/src/models/entry_type.dart';
 import 'package:app/src/models/account.dart';
 import 'package:app/src/models/category.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:money/money.dart';
 
 part "serializers.g.dart";
 
 @SerializersFor(
     const [Category, Account, AccountType, Entry, EntryType, Money, Currency])
-final Serializers serializers =
-    (_$serializers.toBuilder()..addPlugin(StandardJsonPlugin())).build();
+final Serializers serializers = (_$serializers.toBuilder()
+      ..addPlugin(StandardJsonPlugin())
+      ..addPlugin(DateTimeToTimestampSerializerPlugin()))
+    .build();
+
+class DateTimeToTimestampSerializerPlugin implements SerializerPlugin {
+  @override
+  Object beforeSerialize(Object object, FullType specifiedType) {
+    return object;
+  }
+
+  @override
+  Object afterSerialize(Object object, FullType specifiedType) {
+    if (specifiedType.root == DateTime)
+      return Timestamp.fromMicrosecondsSinceEpoch(object);
+
+    return object;
+  }
+
+  @override
+  Object beforeDeserialize(Object object, FullType specifiedType) {
+    if (object is DateTime && specifiedType.root == DateTime)
+      return object.microsecondsSinceEpoch;
+
+    return object;
+  }
+
+  @override
+  Object afterDeserialize(Object object, FullType specifiedType) {
+    return object;
+  }
+}
