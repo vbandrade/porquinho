3e28af95ed4bb5de20c93cbb529bbb21c8b5c93e
@vbandrade <vbandrade@gmail.com>
2019-01-03 20:41:03 -0200

    criando scafold

--- app/lib/src/screens/home.dart
+++ app/lib/src/screens/home.dart
@@ -1,10 +1,33 @@
+import 'package:app/src/widgets/left_drawer.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            color: Colors.white,
+            onPressed: null,
+          ),
+          IconButton(
+            icon: Icon(Icons.fiber_new),
+            color: Colors.white,
+            onPressed: null,
+          ),
+          IconButton(
+            icon: Icon(Icons.calendar_today),
+            color: Colors.white,
+            onPressed: null,
+          ),
+        ],
+      ),
+      drawer: SideDrawer(),
+      body: Container(
+        color: Colors.green,
+      ),
     );
   }
 }
