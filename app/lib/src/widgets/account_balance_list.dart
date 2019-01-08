e34b27102e3d94c49225d351d5c54fb4fae87de0
@vbandrade <vbandrade@gmail.com>
2019-01-07 22:21:43 -0200

    exibindo total

--- app/lib/src/widgets/account_balance_list.dart
+++ app/lib/src/widgets/account_balance_list.dart
@@ -1,25 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:money/money.dart';
 import 'package:app/src/blocs/home_bloc.dart';
 
 class AccountBalanceList extends StatelessWidget {
   final List<AccountBalance> list;
+  final Money totalBalance;
 
-  const AccountBalanceList(this.list);
+  AccountBalanceList(this.list)
+      : totalBalance = list.fold<Money>(
+            Money.fromDouble(0.0, Currency.fromCode("BRL")),
+            (Money previousValue, AccountBalance element) =>
+                previousValue + element.amount);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: list.length,
-      itemBuilder: (BuildContext context, int index) {
-        AccountBalance ab = list[index];
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        GestureDetector(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                "$totalBalance",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                "saldo",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 14),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: list.length,
+            itemBuilder: (BuildContext context, int index) {
+              AccountBalance ab = list[index];
 
-        return ListTile(
-          title: Text("${ab.acountName}"),
-          leading: Icon(Icons.calendar_today),
-          trailing: Text("${ab.amount}"),
-          onTap: () {},
-        );
-      },
+              return ListTile(
+                title: Text("${ab.acountName}"),
+                leading: Icon(Icons.calendar_today),
+                trailing: Text("${ab.amount}"),
+                onTap: () {},
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
